69cb895720c200774720a51af98237ac42c5954a
zachjohnson <johnsza5@oregonstate.edu>
2025-04-23 14:04:03 -0600

    compare.c exercise

---
+++ compare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1;
+    int num2;
+    printf("Enter an integer for x: ");
+    scanf("%d", &num1);
+    printf("Enter an integer for y: ");
+    scanf("%d", &num2);
+
+    if (num1 < num2)
+    {
+        printf("x is less than y\n");
+    }
+    else if (num1 > num2)
+    {
+        printf("x is greater than y\n");
+    }
+    else if (num1 == num2)
+    {
+        printf("Both numbers are equal.");
+    }
+}
