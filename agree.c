77aa775ad9ded7d8032f5c889c945698ab062a83
zachjohnson <johnsza5@oregonstate.edu>
2025-04-23 15:41:21 -0600

    file update

--- agree.c
+++ agree.c
@@ -7,10 +7,10 @@ int main(void)
     printf("Do you agree? T or F: ");
     scanf(" %c", &input); // Space before %c skips any leftover whitespace
 
-    if (input == 'T') {
+    if (input == 'T' || input == 't') {
         printf("Fantastic! You agree!\n");
     }
-    else {
+    else if (input == 'F' || input == 'f') {
         printf("I'm sorry, we disagree!\n");
     }
 
