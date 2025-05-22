9a4674bf70e2717ca3e7cb2787d4ff2f323b7264
zachjohnson <johnsza5@oregonstate.edu>
2025-05-22 15:28:26 -0700

    enum challenge crushed :-)

---
+++ udemy/enum_challenge.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+enum Company {
+    GOOGLE,
+    FACEBOOK, 
+    XEROX, 
+    YAHOO, 
+    EBAY, 
+    MICROSOFT};
+
+int main()
+{
+    enum Company companyA = XEROX;
+    enum Company companyB = GOOGLE;
+    enum Company companyC = EBAY;
+
+    printf("Here are the requested variables index locations\n");
+    printf("%d\n", companyA);
+    printf("%d\n", companyB);
+    printf("%d\n", companyC);
+}
