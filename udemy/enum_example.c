0a05512fc9b9d610aa1cb7454fcb8b04f48583d9
zachjohnson <johnsza5@oregonstate.edu>
2025-05-22 10:39:29 -0700

    enum

---
+++ udemy/enum_example.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+enum Month {
+    JANUARY = 1,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+};
+
+int main() {
+    enum Month birthMonth = JUNE;
+    printf("My birthday is in the %d month\n", birthMonth);
+    return 0;
+}
