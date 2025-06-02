376b922b5eca1b8b21bd881e47cf8f8362efcbdc
zachjohnson <johnsza5@oregonstate.edu>
2025-06-02 15:08:58 -0700

    6/2/25

--- udemy/weeklypay.c
+++ udemy/weeklypay.c
@@ -3,13 +3,13 @@
 const double basicPayRate = 12.00;
 double overtime = (basicPayRate * 1.5);
 double hoursWorked = 0;
+double weeklyPay = 0.00;
+double weeklyPaywOT = 0.00;
+double fullWeek = 480.00;
+
 
 int main()
 {
-    double weeklyPay = 0.00;
-    double weeklyPaywOT = 0.00;
-    double fullWeek = 480.00;
-
     printf("How many hours did you work this week? Enter a number: ");
     scanf("%lf", &hoursWorked);
 
